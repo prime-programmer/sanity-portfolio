299d051a4fa1ade2003b5f29525acb8fe5b2ee2b
prime-programmer <chibike.obin@gmail.com>
2022-05-04 14:44:59 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Header, Footer, Work, Skills, About } from './container';
+import { Navbar } from './components';
+import './App.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+        <Navbar/>
+        <Header/>
+        <About/>
+        <Work/>
+        <Skills/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+
+
+
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <a href='https://github.com/prime-programmer'> <BsGithub /> </a>
+    </div>
+    
+    <div>
+      <a href='https://www.linkedin.com/in/chibike-ugbam-b16b75ab/'> <BsLinkedin /> </a>
+    </div>
+    
+  </div>
+);
+
+export default SocialMedia;
